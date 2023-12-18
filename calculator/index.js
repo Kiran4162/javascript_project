4a49f453ce07ff43d1708018af2afaf79768cfdf
Kiran Kumar Sahu <146617212+Kiran4162@users.noreply.github.com>
2023-12-18 22:48:02 +0530

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,13 @@
+function clr(){
+    document.getElementById("textarea").value = ''
+}
+
+function display(value){
+    document.getElementById("textarea").value += value;
+}
+
+function calculate(){
+    var value = document.getElementById("textarea").value
+    var answer = eval(value)
+    document.getElementById("textarea").value = answer
+}
